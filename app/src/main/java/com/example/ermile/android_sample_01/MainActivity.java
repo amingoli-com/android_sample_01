a23b311253b895250b898d4eab1a1a8dcd162310
amingoli78 <amin.foli78@gmail.com>
2018-12-14 16:57:29 +0330

    set progress for web view

--- app/src/main/java/com/example/ermile/android_sample_01/MainActivity.java
+++ app/src/main/java/com/example/ermile/android_sample_01/MainActivity.java
@@ -1,19 +1,35 @@
 package com.example.ermile.android_sample_01;
 
 import android.app.Dialog;
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.graphics.drawable.ColorDrawable;
+import android.media.RingtoneManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Handler;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.example.ermile.android_sample_01.network.AppContoroler;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
@@ -23,8 +39,14 @@ import java.net.URL;
 public class MainActivity extends AppCompatActivity {
     public Handler mHandler;
     public boolean continue_or_stop;
+    Toolbar toolbars;
+    int versionCode = BuildConfig.VERSION_CODE;
+    String versionName = BuildConfig.VERSION_NAME;
 
+    ViewPager viewPager;
+    TabLayout tabLayout;
 
+    Web_view oneFragment = new Web_view();
 
 
     @Override
@@ -34,11 +56,76 @@ public class MainActivity extends AppCompatActivity {
         new NetCheck().execute();
         // Chek net every 5 seconds
         mHandler = new Handler(); continue_or_stop = true; new Thread(new Runnable() { @Override public void run() { while (continue_or_stop) { try { Thread.sleep(5000);mHandler.post(new Runnable() {@Override public void run() { new NetCheck().execute(); }}); } catch (Exception e) {} } }}).start();
-
         setContentView(R.layout.activity_main);
 
+        viewPager =  findViewById(R.id.viewPager);
+        tabLayout =  findViewById(R.id.tabLayout);
+        setupViewPager(viewPager);
+        tabLayout.setupWithViewPager(viewPager);
+        toolbars = (Toolbar) findViewById(R.id.toolbars);
+        toolbars.setVisibility(View.GONE);
 
 
+        // JSON
+        JsonObjectRequest req = new JsonObjectRequest(Request.Method.GET, "http://mimsg.ir/app.json", null, new Response.Listener<JSONObject>()
+        {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    int app_version = response.getInt("version");
+                    if (app_version > versionCode){
+                        Notification.Builder nb = new Notification.Builder(MainActivity.this);
+                        Uri alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+                        nb.setContentTitle("بروزرسانی")
+                                .setContentText("نسخه جدید رو دانلود کنید!")
+                                .setTicker("برو دانلود کن دیگه")
+                                .setSmallIcon(android.R.drawable.stat_sys_download)
+                                .setAutoCancel(false)
+                                .setSound(alarmSound);
+                        Notification notif = nb.build();
+                        NotificationManager notifManager = (NotificationManager) MainActivity.this.getSystemService(Context.NOTIFICATION_SERVICE);
+                        notifManager.notify(0, notif);
+
+                        Boolean toolbar = response.getBoolean("toolbar");
+                        String appname = response.getString("name");
+                        if (toolbar == true){
+                            toolbars.setVisibility(View.VISIBLE);
+                            toolbars.setTitle(appname);
+                            setSupportActionBar(toolbars);
+                            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+                        }else {
+                            Toast.makeText(MainActivity.this, "نشد", Toast.LENGTH_SHORT).show();
+                            toolbars.setVisibility(View.GONE);
+                        }
+
+
+
+                    }
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        },new Response.ErrorListener()
+        {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+            }
+        });AppContoroler.getInstance().addToRequestQueue(req);
+        // END JSON
+
+
+
+
+
+    }
+    private void setupViewPager(final ViewPager viewPager)
+    {
+        final Util.ViewPagerAdapter adapter = new Util.ViewPagerAdapter(getSupportFragmentManager());
+
+        adapter.addFragment(oneFragment,"one");
+        viewPager.setAdapter(adapter);
     }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
--- app/src/main/java/com/example/ermile/android_sample_01/Web_view.java
+++ app/src/main/java/com/example/ermile/android_sample_01/Web_view.java
@@ -1,11 +1,31 @@
 package com.example.ermile.android_sample_01;
 
 
+import android.app.Dialog;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.example.ermile.android_sample_01.network.AppContoroler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 /**
@@ -23,7 +43,40 @@ public class Web_view extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.web_view, container, false);
+        View web_view_fragment = inflater.inflate(R.layout.web_view, container, false);
+
+        final ProgressBar progressBar = web_view_fragment.findViewById(R.id.progress_webview);
+        final WebView webView = web_view_fragment.findViewById(R.id.web_view);
+        webView.setWebViewClient(new WebViewClient());
+
+
+
+        // JSON
+        JsonObjectRequest req = new JsonObjectRequest(Request.Method.GET, "http://mimsg.ir/app.json", null, new Response.Listener<JSONObject>()
+        {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                 String url = response.getString("web");
+                 if (webView ==null){
+                     progressBar.setVisibility(View.VISIBLE);
+                 }else {progressBar.setVisibility(View.GONE);}
+                 webView.loadUrl(url);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        },new Response.ErrorListener()
+        {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+            }
+        });AppContoroler.getInstance().addToRequestQueue(req);
+        // END JSON
+        return web_view_fragment;
+
     }
+
 
 }
